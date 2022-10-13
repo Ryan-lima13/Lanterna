b4fcdb96cb468628581df130c3b2b14d2a069e02
ryanl <ryanlimadev@gmail.com>
2022-10-13 10:19:20 -0300

    criando tela principal

--- app/src/main/java/com/rlds/lanterna/MainActivity.kt
+++ app/src/main/java/com/rlds/lanterna/MainActivity.kt
@@ -2,10 +2,14 @@ package com.rlds.lanterna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rlds.lanterna.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar!!.hide()
     }
 }
